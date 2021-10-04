053b3eefce23a8f9c019bdc2cce8b78702788494
kayserpc <kayserpc@gmail.com>
2021-10-04 13:36:29 -0300

    quinto commit

--- server.js
+++ server.js
@@ -39,13 +39,13 @@ const port = process.env.PORT || 5000;
   // process.env.port is Heroku's port if you choose to deploy the app there
   app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
-  //if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-  //  app.use(express.static('client/public'));
+    app.use(express.static('client/public'));
   
     // Express serve up index.html file if it doesn't recognize route
-  //  const path = require('path');
-  //  app.get('*', (req, res) => {
-  //    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-  //  });
- // }
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+    });
+  }
