d4e37382b5e27e3a88c56ef0dccf7230e936bed4
kayserpc <kayserpc@gmail.com>
2021-10-05 15:13:50 -0300

    tercer commit 05/10

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 
 const users = require("./routes/api/users");
 
@@ -33,8 +34,21 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
-  
+
+//serve assets if in production
+if (process.env.NODE_ENV === 'production') {
+// adding routes
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000; 
   
   // process.env.port is Heroku's port if you choose to deploy the app there
   app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
